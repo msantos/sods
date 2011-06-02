8331a2ddf8f5cecb5ffc9920e76f5feee3a1c445
Michael Santos <michael.santos@gmail.com>
2011-06-02 12:44:29 -0400

    Update copyright date

--- sdt/sdt.h
+++ sdt/sdt.h
@@ -1,7 +1,7 @@
 /*
  * Socket over DNS client.
  *
- * Copyright (c) 2009 Michael Santos <michael.santos@gmail.com>
+ * Copyright (c) 2009-2011 Michael Santos <michael.santos@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
